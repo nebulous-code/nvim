8513554c71431bcf9fa7548fbc8b14ac496fe118
Nicholas Licalsi <licalsinj@gmail.com>
2026-04-17 14:56:33 -0500

    changed up git labels

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -3,13 +3,28 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
 	require("nvim-tree").setup({
-    		actions = {
-			open_file = {
-				window_picker = {
-					enable = false,
+    	actions = {
+			  open_file = {
+				  window_picker = {
+					  enable = false,
 				},
 			},
 		},
+    renderer = {
+      icons = {
+        glyphs = {
+          git = {
+            unstaged = "~",
+            staged = "✓",
+            unmerged = "⌥",
+            renamed = "➜",
+            untracked = "?",
+            deleted = "✗",
+            ignored = "◌",
+          }
+        }
+      }
+    }
 	})
     vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file tree" })
   end,
