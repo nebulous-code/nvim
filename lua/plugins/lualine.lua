38ec3181cbf0848749074aa1ea834c8da1044df1
Nicholas Licalsi <licalsinj@gmail.com>
2026-08-15 14:40:43 -0500

    fixed some formatting in markdown

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,24 @@ return {
       options = {
         theme = "auto",
       },
+      sections = {
+        lualine_x = {
+          {
+            function()
+              if vim.fn.mode():find("[vV]") then
+                return vim.fn.wordcount().visual_words .. " words"
+              end
+              return vim.fn.wordcount().words .. " words"
+            end,
+            cond = function()
+              return vim.bo.filetype == "markdown"
+            end,
+          },
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+      },
     })
   end,
 }
